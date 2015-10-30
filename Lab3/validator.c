37bdd9e31daac0a00f96e0bbeaccbf9e02c37973
jerkwok <jeremy.kwok@uoit.net>
2015-10-30 12:46:03 -0400

    update to validator

--- Lab3/validator.c
+++ Lab3/validator.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <pthread.h>
 
+// pthread_mutex_t mutex;
 
 /* structure for passing data to threads */
 typedef struct
@@ -16,6 +17,7 @@ typedef struct
 
 int *validate_row(void *arg){
 	parameters* data = (parameters*) arg;
+	int myselector = data -> selector;
 	// int* rownum = (int*) arg;
 	//used track tells which numbers have been used in this row
 	int used[10] = {0};
@@ -24,14 +26,14 @@ int *validate_row(void *arg){
 	for (int i = 0; i < 9; i++)
 	{
 		//if the element is nonzero (actual data)
-		int currElement = data -> sudokuGrid[data -> selector][i];
+		int currElement = data -> sudokuGrid[myselector][i];
 		if (currElement != 0)
 		{
 			//if the element has already been used this row
 			if (used[currElement] == 1)
 			{
 				//row is not valid, return false 0
-				printf("Row %d Not Valid\n", data -> selector);
+				printf("Row %d Not Valid\n", myselector);
 				return 0;
 			}else{
 				used[currElement] = 1;
@@ -39,12 +41,13 @@ int *validate_row(void *arg){
 		}
 	}
 	//row is valid, return true 1;
-	printf("Row %d Valid\n", data -> selector);
+	printf("Row %d Valid\n", myselector);
 	return 1;
 }
 
 int *validate_col(void *arg){
 	parameters* data = (parameters*) arg;
+	int myselector = data -> selector;
 	//used track tells which numbers have been used in this col
 	int used[10] = {0};
 
@@ -52,14 +55,14 @@ int *validate_col(void *arg){
 	for (int i = 0; i < 9; i++)
 	{
 		//if the element is nonzero (actual data)
-		int currElement = data -> sudokuGrid[i][data -> selector];
+		int currElement = data -> sudokuGrid[i][myselector];
 		if (currElement != 0)
 		{
 			//if the element has already been used this col
 			if (used[currElement] == 1)
 			{
 				//col is not valid, return false 0
-				printf("Col %d Not Valid\n", data -> selector);
+				printf("Col %d Not Valid\n", myselector);
 				return 0;
 			}else{
 				used[currElement] = 1;
@@ -67,12 +70,13 @@ int *validate_col(void *arg){
 		}
 	}
 	//cpl is valid, return true 1;
-	printf("Col %d Valid\n", data -> selector);
+	printf("Col %d Valid\n", myselector);
 	return 1;
 }
 
 int *validate_box(void *arg){
 	parameters* data = (parameters*) arg;
+	int myselector = data -> selector;
 	//used track tells which numbers have been used in this box
 	int used[10] = {0};
 
@@ -90,7 +94,7 @@ int *validate_box(void *arg){
 			//data -> selector => 2 5 8 start at col 6
 
 			//if the element is nonzero (actual data)
-			int currElement = data -> sudokuGrid[i+(data -> selector/3)*3][j+(data -> selector%3)*3];
+			int currElement = data -> sudokuGrid[i+(myselector/3)*3][j+(myselector%3)*3];
 			// printf("row:%d col:%d\n",i+(data -> selector/3)*3,j+(data -> selector%3)*3 );
 			if (currElement != 0)
 			{
@@ -98,7 +102,7 @@ int *validate_box(void *arg){
 				if (used[currElement] == 1)
 				{
 					//box is not valid, return false 0
-					printf("Box %d Not Valid\n", data -> selector);
+					printf("Box %d Not Valid\n", myselector);
 					return 0;
 				}else{
 					used[currElement] = 1;
@@ -107,22 +111,29 @@ int *validate_box(void *arg){
 		}
 	}
 	//box is valid, return true 1;
-	printf("Box %d Valid\n", data -> selector);
+	printf("Box %d Valid\n", myselector);
 	return 1;
 }
 
 int main(int argc,char *argv[])
 {
+	//init threads
 	pthread_t rowthread;
 	pthread_t colthread;
 	pthread_t boxthread;
-	parameters *data = (parameters *) malloc(sizeof(parameters)); 
+
+	//init data structure
+	parameters *data = (parameters *) malloc(sizeof(parameters));
+
+	//init mutex
+	// pthread_mutex_init(&mutex,NULL); 
 
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
-			data->sudokuGrid[i][j] = i;
+			// data->sudokuGrid[i][j] = i;
+			data->sudokuGrid[i][j] = 0;
 		}
 	}
 
@@ -137,21 +148,27 @@ int main(int argc,char *argv[])
 
 
 	//Test Cases
+	// pthread_mutex_lock (&mutex);
 	//Validate Box 4
 	data -> selector = 4;
 	pthread_create(&boxthread, 0, validate_box, (void *) data);
+	(void) pthread_join(boxthread,NULL);
+	// pthread_mutex_unlock (&mutex);
 
+	// pthread_mutex_lock (&mutex);
 	//Validate Row 7
 	data -> selector = 7;
 	pthread_create(&rowthread, 0, validate_row, (void *) data);
+	(void) pthread_join(rowthread,NULL);
+	// pthread_mutex_unlock (&mutex);
 
+	// pthread_mutex_lock (&mutex);
 	//Validate Col 1
 	data -> selector = 1;
 	pthread_create(&colthread, 0, validate_col, (void *) data);
+	(void) pthread_join(colthread,NULL);
+	// pthread_mutex_unlock (&mutex);
 
 	//join all threads
-	(void) pthread_join(boxthread,NULL);
-	(void) pthread_join(rowthread,NULL);
-	(void) pthread_join(colthread,NULL);
   return 0;
 }
